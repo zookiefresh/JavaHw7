cea6d023486fc001aae70c1284acd196feb71e3a
Duy Nguyen <salesnguyen1@gmail.com>
2015-02-20 01:45:00 -0800

    Add electric car class methods

--- src/carControl/ElectricCar.java
+++ src/carControl/ElectricCar.java
@@ -10,26 +10,36 @@ package carControl;
  * @author duy
  */
 public class ElectricCar implements CarControl {    
+    
+    private String powered;
+    private int direction;
+    private int speed;
+    
+    ElectricCar(){
+        speed = 0;
+        direction = 365;
+    }
+    
     public void powerOn(){
-        
+        powered = "On";
     }
     public void powerOff(){
-        
+        powered = "Off";
     }
     
     public void turnRight(){
-        
+        direction = direction - 5; 
     }
     
     public void turnLeft(){
-        
+        direction = direction + 5;        
     }
     
     public void goFaster(){
-        
+        speed += 5;        
     }
     
     public void slowDown(){
-        
+        speed -= 5;
     }
 }
